7d2ff5b1867d513a8eeedfe42472bcdf3c7232bf
Alejandro Sevilla <alejandro.sevilla@iesdoctorbalmis.com>
2021-11-04 14:48:30 +0100

    Agregar archivos de proyecto.

---
+++ MemeMaker/Conversores/BordeConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    class BordeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool marcado = (bool)value;
+            if (marcado)
+            {
+                return 2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 0;
+        }
+    }
+}
